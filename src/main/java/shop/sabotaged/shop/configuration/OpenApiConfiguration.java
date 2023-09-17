c6f2237d3488fe3b831f397d9b50eea98a78c6b2
ronald1-1-1 <mishakazanin@mail.ru>
2023-09-17 20:46:49 +0300

    - Remove unused imports

--- src/main/java/shop/sabotaged/shop/configuration/OpenApiConfiguration.java
+++ src/main/java/shop/sabotaged/shop/configuration/OpenApiConfiguration.java
@@ -1,14 +1,7 @@
 package shop.sabotaged.shop.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springdoc.core.customizers.OperationCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Optional;
 
 @OpenAPIDefinition(
         info = @Info(
